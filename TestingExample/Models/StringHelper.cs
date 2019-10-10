a2490c0bc89f327aaa2fd9cbe5e1fb1d6d73fbac
Kevin Stone <ruca6616@gmail.com>
2019-10-09 19:00:57 -0700

    Add project files.

---
+++ TestingExample/Models/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingExample.Models
+{
+    public static class StringHelper
+    {
+
+        public static bool IsTextFile(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                return false;
+            }
+            fileName = fileName.ToLower();
+            if (!fileName.EndsWith(".txt"))
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+
+    }
+}
---
+++ TestingExampleTests/Models/StringHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingExample.Models.Tests
+{
+    [TestClass()]
+    public class StringHelperTests
+    {
+        [TestMethod()]
+        public void IsTextFile_ValidTextFile_ReturnsTrue()
+        {
+            // arrange
+            string fileName = "joe.txt";
+
+            // act
+            bool result = StringHelper.IsTextFile(fileName);
+
+            // assert
+            Assert.IsTrue(result);
+
+        }
+    }
+}
